2c5a3f7384799aa2a12053a7fd4c6606e468edee
gwakdongho <rhkrtjdwls1@naver.com>
2025-10-30 13:19:45 +0900

    [새 프로젝트 생성] SOH 프로젝트 생성

--- Source/ch6/ch6.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ch6.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ch6, "ch6" );
-
-DEFINE_LOG_CATEGORY(Logch6)
--- Source/ch6/ch6GameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ch6GameMode.h"
-
-Ach6GameMode::Ach6GameMode()
-{
-	// stub
-}
