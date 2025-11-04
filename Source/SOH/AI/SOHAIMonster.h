1b0b55cbd637053c3acfe2c2cd293e9b2ad7e124
gwakdongho <rhkrtjdwls1@naver.com>
2025-11-04 11:23:34 +0900

    [추가] AIPerception 기능 추가

--- Source/SOH/AI/SOHAIMonster.h
+++ Source/SOH/AI/SOHAIMonster.h
@@ -14,14 +14,39 @@ class SOH_API ASOHAIMonster : public ACharacter
 public:
 	ASOHAIMonster();
 
+
 protected:
 	virtual void BeginPlay() override;
 
-public:
 
+public:
+	/** 순찰 지점 배열 (에디터에서 배치 후 할당) */
 	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "AI|Patrol")
 	TArray<ATargetPoint*> PatrolTargets;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Movement")
 	float PatrolSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Movement")
+	float ChaseSpeed;
+
+	// 시야
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Perception|Sight")
+	float SightRadius;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Perception|Sight")
+	float LoseSightRadius;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Perception|Sight")
+	float PeripheralVisionAngle;
+
+
+	//청각
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Perception|Hearing")
+	float HearingRange;
+
+	UFUNCTION(BlueprintCallable, Category = "AI|Movement")
+	void SetMoveSpeed(float NewSpeed);
 };
--- Source/SOH/AI/SOHAIMonsterBTDecorator.cpp
+++ Source/SOH/AI/SOHAIMonsterBTDecorator.cpp
@@ -6,11 +6,11 @@ USOHAIMonsterBTDecorator::USOHAIMonsterBTDecorator()
 	NodeName = TEXT("Check Player In Range");
 }
 
+
 bool USOHAIMonsterBTDecorator::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
 {
 	UBlackboardComponent* BB = OwnerComp.GetBlackboardComponent();
 	if (!BB) return false;
 
-	const bool bPlayerInRange = BB->GetValueAsBool(TEXT("PlayerInRange"));
-	return bPlayerInRange;
+	return BB->GetValueAsBool(TEXT("PlayerInRange"));
 }
