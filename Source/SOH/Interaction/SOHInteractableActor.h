ff667de1a14e0daf73cd4bf7e15e5c5313b7650e
KANGTAKWAN <xkvk2012@gmail.com>
2025-11-18 12:13:19 +0900

    [개선] 인터랙터블 액터 기능 구현

--- Source/SOH/Interaction/SOHInteractableActor.h
+++ Source/SOH/Interaction/SOHInteractableActor.h
@@ -4,6 +4,8 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "SOHInteractInterface.h"
+#include "Components/MeshComponent.h"
+#include "Blueprint/UserWidget.h"
 #include "SOHInteractableActor.generated.h"
 
 UCLASS()
@@ -15,9 +17,21 @@ public:
 	ASOHInteractableActor();
 
 protected:
-	virtual void BeginPlay() override;
 
 	virtual void Interact_Implementation(AActor* Caller) override;
-	virtual bool CanReceiveTrace_Implementation(AActor* Caller) override;
+	virtual bool CanReceiveTrace_Implementation(AActor* Caller, bool bCanInteract) override;
+	
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	void ShowInteractWidget();
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	void HideInteractWidget();
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	void ApplyOverlayMaterial(UMaterialInterface* Material);
 
+	UPROPERTY(EditDefaultsOnly, Category = "Interaction|UI")
+	TSubclassOf<UUserWidget> InteractionWidgetClass;
+	UPROPERTY()
+	TObjectPtr<UUserWidget> InteractionWidgetInstance;
+	UPROPERTY(EditDefaultsOnly, Category = "Interaction|Visual")
+	TObjectPtr<UMaterialInterface> OutlineMaterial;
 };
