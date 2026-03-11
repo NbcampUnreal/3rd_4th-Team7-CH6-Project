bd021b601f32d4f6003a04343cad919442827863
Hyunsu Jung <freedomquest88@naver.com>
2026-03-11 13:03:09 +0900

    [수정] 컷씬 플레이어 C++ 코드에 안정성 구조 추가

--- Source/SOH/GameMode/SOHCutscenePlayerBase.cpp
+++ Source/SOH/GameMode/SOHCutscenePlayerBase.cpp
@@ -1,8 +1,19 @@
 #include "GameMode/SOHCutscenePlayerBase.h"
 
+#include "Blueprint/UserWidget.h"
+
 // Sets default values
 void ACutscenePlayerBase::PlayCutscene_Implementation()
 {
 	
 }
 
+void ACutscenePlayerBase::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (ActiveWidget)
+	{
+		ActiveWidget->RemoveFromParent();
+		ActiveWidget = nullptr;
+	}
+	Super::EndPlay(EndPlayReason);
+}
--- Source/SOH/GameMode/SOHCutscenePlayerBase.h
+++ Source/SOH/GameMode/SOHCutscenePlayerBase.h
@@ -10,7 +10,17 @@ class SOH_API ACutscenePlayerBase : public AActor
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Cutscene")
+	TSoftClassPtr<UUserWidget> CutScenePlayerWidgetClass;
+
+	// 생성된 위젯을 추적하기 위한 변수
+	UPROPERTY(Transient)
+	UUserWidget* ActiveWidget;
+	
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Cutscene")
 	void PlayCutscene();
+
+	// 레벨이 언로드 될 때 위젯을 정리하기 위한 오버라이드
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 };
 
